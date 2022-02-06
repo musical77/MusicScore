880b55c82331e237bcea03bfe1b3d0ff11a7bce6
live77 <live77>
2022-02-06 20:56:30 +0800

    refine package dependency

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(name: "MusicSymbol", url: "https://github.com/musical77/MusicSymbol.git", from: "0.0.0"),
+        .package(url: "https://github.com/musical77/MusicSymbol.git", .branch("main")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
