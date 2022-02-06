170726021f4c4e76dd06532f940259c1b739bf27
live77 <live77>
2022-02-06 20:47:45 +0800

    declare package dependency

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/musical77/MusicSymbol.git", from: "0.0.0"),
+        .package(name: "MusicSymbol", url: "https://github.com/musical77/MusicSymbol.git", from: "0.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
