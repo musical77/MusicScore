dd52d01a9709fd25c4507472fc452490936db840
live77 <live77>
2022-02-10 23:18:03 +0800

    remove instrument type

--- Tests/MusicScoreTests/Parser/MIDIScoreParserTests.swift
+++ Tests/MusicScoreTests/Parser/MIDIScoreParserTests.swift
@@ -1,18 +1,15 @@
-//
 //  MIDIScoreParserTests.swift
-//  
 //
 //  Created by lively77 on 2022/2/9.
-//
 
 import Foundation
 import MusicScore
 import XCTest
 
 class MIDIScoreParserTests : XCTestCase {
+    
     func testScoreLoadFromURL() {
-       
-        let score1 = MIDIScoreParser().loadMusicScore(url: ScoreSamples.url_spring1st)!
-        print(score1.musicParts[2])
+        let score = MIDIScoreParser().loadMusicScore(url: ScoreSamples.url_spring1st)
+        XCTAssertNotNil(score)
     }
 }
